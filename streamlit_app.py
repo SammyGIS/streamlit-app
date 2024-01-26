1d1a31d1f12dcb5de566e40ad6b45c7e822e4bb8
SammyGIS <adedoyinsamuel25@gmail.com>
2024-01-26 22:11:45 +0100

    add requirements

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# set title
+st.title("hello word")
+
+# write on the page
+st.write("A streamlit demo")
+# instead of st write you can pass your text directt
+"This is a test"
