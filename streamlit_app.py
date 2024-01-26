d4c14c45893fc9bca10d930082babf3977df5571
SammyGIS <adedoyinsamuel25@gmail.com>
2024-01-26 22:18:37 +0100

    adding text to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,36 @@ st.title("hello word")
 st.write("A streamlit demo")
 # instead of st write you can pass your text directt
 "This is a test"
+
+#"**bold**"
+# "*italic*"
+# markdown
+st.markdown("streamlit is **very** *cool*")
+
+#header
+st.header("This is a header")
+st.subhead("This sis a subheader")
+st.caption("This is caption")
+
+# to represent blcok of code
+
+st.code("""
+import streamlit as st
+
+# set title
+st.title("hello word")
+
+# write on the page
+st.write("A streamlit demo")
+# instead of st write you can pass your text directt
+"This is a test"
+
+#"**bold**"
+# "*italic*"
+# markdown
+st.markdown("streamlit is **very** *cool*")
+
+#header
+st.header("This is a header")
+st.subhead("This sis a subheader")
+ """)
