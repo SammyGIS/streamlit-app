c4b982a461a1d5f66c9c9dfe1af7fcf0c9c8d577
SammyGIS <adedoyinsamuel25@gmail.com>
2024-01-27 01:06:05 +0100

    add metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,33 +3,23 @@ import pandas as pd
 
 
 APP_TITLE = "Fraud and Identity Theft Report"
-APP_SUB_TITLE = 'Source" Federal Trade Commission'
+APP_SUB_TITLE = 'Source: Federal Trade Commission'
 
-def display_fraud_facts(year,quarter,state_name,report_type,
-                         field_name,metric_title):
+def display_fraud_facts(df,year,quarter,state_name,report_type,
+                         field_name,metric_title,
+                         currency_sign='', is_median=False):
     
-    metric_title = f'# {report_type} of Reports'
-
     df = df[(df['Year'] == year) & (df['Quarter'] == quarter)\
              & (df['Report Type'] == report_type)]
     if state_name:
         df=df[df['State Name']==state_name]
     df.drop_duplicates(inplace =True)
-    total = df[field_name].sum()
-    st.metric(metric_title,'{:,}'.format(total))
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if is_median:
+        total = df[field_name].sum()/len(df) \
+            if len(df) else 0
+    else:
+        total = df[field_name].sum()
+    st.metric(metric_title,'{}{:,}'.format(currency_sign,round(total)))
 
 def main():
     st.set_page_config(APP_TITLE)
@@ -37,22 +27,17 @@ def main():
     st.caption(APP_SUB_TITLE)
 
     # load Data
-    df = pd.read_csv('data\AxS-Fraud Box_Full Data_data.csv')
+    df_fraud= pd.read_csv('data\AxS-Fraud Box_Full Data_data.csv')
+    df_median = pd.read_csv('data\AxS-Median Box_Full Data_data.csv')
+    df_loss = pd.read_csv('data\AxS-Losses Box_Full Data_data.csv')
+
 
     year   = 2022
     quarter = 1
-    state_name = 'Texas'
+    state_name = ''
     report_type = 'Fraud'
-    field_name  = 'State Fraud/Other Count'
-
-    display_fraud_facts(year,quarter,state_name,report_type,
-                         field_name,metric_title)
-
-    st.write(df.head())
-    st.write(df.columns)
-
-    
-
+    metric_title = f'# {report_type} of Reports'
+    currency_sign = '$'
 
 
     # Display filters and Map
@@ -61,6 +46,19 @@ def main():
 
 
     # Display Metrics
+    st.subheader(f'{state_name} {report_type} Facts')
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        display_fraud_facts(df_fraud, year,quarter,state_name,report_type,
+                         'State Fraud/Other Count',metric_title)
+    with col2:
+        display_fraud_facts(df_median, year,quarter,state_name,report_type,
+                         'Overall Median Losses Qtr','Median $ loss',
+                         currency_sign, is_median=True)
+    with col3:
+        display_fraud_facts(df_loss, year,quarter,state_name,report_type,
+                         'Total Losses','Total $ loss',currency_sign)
+
 
 
 if __name__ =="__main__":
